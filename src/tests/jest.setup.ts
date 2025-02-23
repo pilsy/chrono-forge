30d73e9cc9fb3395acbe1ed9f955d72be13903de
Richard <richard.gustin86@gmail.com>
2025-02-23 19:52:27 +1000

    Update jest.setup.ts

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,10 +1,5 @@
 import { setup } from './setup';
-import { teardown } from './teardown';
 
 beforeAll(async () => {
   await setup();
-}, 30000);
-
-afterAll(async () => {
-  await teardown();
 }, 30000);
